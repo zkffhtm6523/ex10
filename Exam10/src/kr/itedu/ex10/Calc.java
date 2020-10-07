451cb593e51d11960c153bbef50365495875d7a9
david <zkffhtm6523@naver.com>
2020-10-07 11:23:07 +0900

    2020.10.07 깃허브 워크스페이스 2개 연동

--- Exam10/src/kr/itedu/ex10/Calc.java
+++ Exam10/src/kr/itedu/ex10/Calc.java
@@ -4,5 +4,8 @@ public class Calc {
 	//n1, n2를 더한 값을 리턴해야 함
 	public static int sum(int n1, int n2) {
 		return n1 + n2;
-	}	
+	}
+	public static int minus(int n1, int n2) {
+		return n1 - n2;
+	}
 }
